f8b7962c7f2c972238426e9028a8199bc5187fc1
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2024-10-28 00:30:25 -0300

    Agregar EndParking a IParkingService

--- Services/IParkingService.cs
+++ Services/IParkingService.cs
@@ -4,6 +4,7 @@ namespace Services
 {
     public interface IParkingService
     {
+        bool EndParking(ESPCloseParkingDto dto);
         IEnumerable<ESPParkingDto> GetActives();
         IEnumerable<ESPParkingDto> GetAll();
         int StartParking(ESPOpenParkingDto dto);
